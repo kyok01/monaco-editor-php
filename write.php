d4311cda17aa74d16ee425144bb6ad8242f8c3b7
kyok01 <kazutakayokoils@gmail.com>
2022-06-01 10:24:10 +0900

    一時保存

---
+++ write.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+$age = $_POST["age"];
+$c = ",";
+
+
+?>
+
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>File書き込み</title>
+</head>
+<body>
+
+<h1>書き込みしました。</h1>
+<h2>./data/test.txt を確認しましょう！</h2>
+
+<ul>
+<li><a href="input.php">戻る</a></li>
+</ul>
+</body>
+</html>
